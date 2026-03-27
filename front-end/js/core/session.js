48e234dcba4c8bab79222b2ce5883c7df41d2257
Harsh Raj <rajharsh797864@gmail.com>
2026-03-27 18:16:17 +0530

    feat: Design login page and automatic dashboard display

---
+++ front-end/js/core/session.js
@@ -0,0 +1,24 @@
+function getSession(){
+
+return JSON.parse(
+
+localStorage.getItem("endurSession")
+
+);
+
+}
+
+
+function requireAuth(){
+
+const session = getSession();
+
+if(!session){
+
+window.location.href = "../../login.html";
+
+}
+
+return session;
+
+}
